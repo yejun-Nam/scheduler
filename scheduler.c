13b174e57bf8ee2b22ceedc60e2da9b5fe269532
yejun-Nam <yejun1148@hanyang.ac.kr>
2022-06-19 22:28:01 +0900

    sjf ntt 에러수정

--- scheduler.c
+++ scheduler.c
@@ -312,7 +312,7 @@ int main() {
 					tat = tat + ready_queue[i].ta_t;
 					waitt = waitt + ready_queue[i].wait_t;
 
-					printf("%d\t%d\t%d\t%d\t%d\n", ready_queue[i].number, ready_queue[i].arrive_t, ready_queue[i].burst_t, ready_queue[i].wait_t, ready_queue[i].ta_t, (double)ready_queue[i].ta_t / ready_queue[i].burst_t);
+					printf("%d\t%d\t%d\t%d\t%d\t%f\n", ready_queue[i].number, ready_queue[i].arrive_t, ready_queue[i].burst_t, ready_queue[i].wait_t, ready_queue[i].ta_t, (double)ready_queue[i].ta_t / ready_queue[i].burst_t);
 				}
 
 				printf("반환시간 평균 : %.2f\n",tat/n);
